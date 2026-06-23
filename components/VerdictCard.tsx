2b3b8c9ddd51120c61a2bbfa634c1865329b37ae
Christian R. Gomelan <100290998+chrisgomelan@users.noreply.github.com>
2026-06-23 09:34:47 +0800

    initial setup for file structure

---
+++ components/VerdictCard.tsx
@@ -0,0 +1,11 @@
+
+export default function VerdictCard(){
+    return(
+        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+                <h2 className="text-2xl font-bold text-black dark:text-zinc-50">Verdict Card</h2>
+                <p className="text-lg text-zinc-600 dark:text-zinc-400">
+                    This is the verdict card component.
+                </p>
+            </div>
+    );
+}
